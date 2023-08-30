7771ab733aa993a1d5ad25f059d11565a666ab7e
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-08-30 16:47:10 -0400

    melhorias

--- script.js
+++ script.js
@@ -6,29 +6,70 @@ let password = document.querySelector('#password');
 
 let containerPassword = document.querySelector('#container-password');
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@";
+let charset = {
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  numbers: "0123456789",
+  symbols: "!@#$%&*"
+};
+
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function(){
+slider.oninput = function () {
   sizePassword.innerHTML = this.value;
-}
+};
 
-function generationPassword(){
+function generationPassword() {
   let pass = "";
+  let selectedCharsets = "";
 
-  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
-    pass += charset.charAt(Math.floor(Math.random() * n))
+  let includeUppercase = document.querySelector('#uppercase').checked;
+  let includeLowercase = document.querySelector('#lowercase').checked;
+  let includeNumbers = document.querySelector('#numbers').checked;
+  let includeSymbols = document.querySelector('#symbols').checked;
+
+  if (includeUppercase) {
+    selectedCharsets += charset.uppercase;
+  }
+  if (includeLowercase) {
+    selectedCharsets += charset.lowercase;
+  }
+  if (includeNumbers) {
+    selectedCharsets += charset.numbers;
+  }
+  if (includeSymbols) {
+    selectedCharsets += charset.symbols;
+  }
+
+  if (selectedCharsets.length === 0) {
+    alert("Please select at least one character set.");
+    return;
+  }
+
+  let n = selectedCharsets.length;
+
+  for (let i = 0; i < sliderElement.value; ++i) {
+    pass += selectedCharsets.charAt(Math.floor(Math.random() * n));
   }
 
   containerPassword.classList.remove("hide");
-  password.innerHTML = pass; 
+  password.innerHTML = pass;
   novaSenha = pass;
 }
 
-function copyPassword(){
-  alert ("Senha copiada com sucesso!")
-  navigator.clipboard.writeText(novaSenha);
+function copyPassword() {
+  if (novaSenha) {
+    navigator.clipboard.writeText(novaSenha)
+      .then(() => {
+        alert("Password copied successfully!");
+      })
+      .catch((error) => {
+        alert("Error copying password: " + error);
+      });
+  } else {
+    alert("Generate a password first.");
+  }
 }
 
